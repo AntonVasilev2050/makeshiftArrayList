1c25d3c4e31d206122e41ca97f6701117c7511d4
anton <avvsoft2050@gmail.com>
2021-12-30 18:31:59 +0300

    make makeshiftArrayList interface

---
+++ src/main/java/makeshiftArrayList.java
@@ -0,0 +1,9 @@
+public interface makeshiftArrayList {
+    String get (int index);
+    void add(String string);
+    boolean remove(String string);
+    boolean removeAt(int index);
+    int size();
+    void clear();
+
+}
