2c1f5ea51817259917600433bdb163db8ffafbe5
anton <avvsoft2050@gmail.com>
2021-12-31 13:15:13 +0300

    add tests
    whenStringRemovedThenSizeMustBeDecreased()
    and
    whenNonExistentStringRemovedThenReturnFalse()

--- src/test/java/makeshiftArrayListTest.java
+++ src/test/java/makeshiftArrayListTest.java
@@ -9,23 +9,35 @@ class makeshiftArrayListTest {
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
         // init
-    }
-
-    @Test
-    public void whenAdded100StringsThenSizeMustBe100(){
-       for (int i = 0; i < 100; i++){
-           makeshiftArrayList.add("string-" + i);
-       }
-       assertEquals(100, makeshiftArrayList.size());
-    }
-
-    @Test
-    public void whenStringRemovedByIndexThenSizeMustBeDecreased(){
-        for (int i = 0; i < 100; i++){
+        for (int i = 0; i < 100; i++) {
             makeshiftArrayList.add("string-" + i);
         }
+    }
+
+    @Test
+    public void whenAdded100StringsThenSizeMustBe100() {
         assertEquals(100, makeshiftArrayList.size());
+    }
+
+    @Test
+    public void whenStringRemovedByIndexThenSizeMustBeDecreased() {
         assertTrue(makeshiftArrayList.removeAt(5));
         assertEquals(99, makeshiftArrayList.size());
+    }
+
+    @Test
+    public void whenStringRemovedThenSizeMustBeDecreased(){
+        String stringForTest = "testString";
+        makeshiftArrayList.add(stringForTest);
+        assertEquals(101, makeshiftArrayList.size());
+        assertTrue(makeshiftArrayList.remove(stringForTest));
+        assertEquals(100, makeshiftArrayList.size());
+    }
+
+    @Test
+    public void whenNonExistentStringRemovedThenReturnFalse(){
+        String stringForTest = "testString";
+        assertFalse(makeshiftArrayList.remove(stringForTest));
+        assertEquals(100, makeshiftArrayList.size());
     }
 }
